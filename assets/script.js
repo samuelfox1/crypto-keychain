0377fbf1dc1387f13b066f1d4020ae4a438bd198
Sam Fox <samueljasonfox@gmail.com>
2020-12-10 22:53:38 -0800

    re-designed the javascript generator. user selects character types, then engine picks a random type and a random character to build a new password

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,152 @@
-//================PASSWORD GENERATOR========================
+//================PASSWORD GENERATOR=================//
 
-// establish button variable
+
+//define variables  
+var passwordText = document.querySelector("#password");
+passwordText.value = 'push the red button';
 var generateBtn = document.querySelector("#generate");
-// launch program when button clicked
-generateBtn.addEventListener("click", correctEntry);
+var alphaLowerChars = ('abcdefghijklmnopqrstuvwxyz')
+var alphaUpperChars = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+var numericChars = ('1234567890')
+var specialChars = ('!"#$%&*+-/;<=>?@\^_`|~')
+var password = ''
+var passwordLength = 0
+var selectionsArray = []
+console.log('program ready')
+
+
+generateBtn.addEventListener("click", function(){
+  //clear variables for looped use
+  
+  selectionsArray=[]
+  password=''
+  console.log('program started')
+  
+  // launch program when button clicked
+  correctEntry()  
+})
 
 
 //edge case, verify legal input values
-function correctEntry() {
+function correctEntry() {    
   // prompt user for input and store value in variable
-  userInput = prompt('enter password length from 8 to 128 characters')
+  userInput = prompt('enter desired password length between 8 and 128 characters')
   //convert user input string to integer and store in variable
   passwordLength = parseInt(userInput)
   //verify legal user prompt input
   if (passwordLength > 7 && passwordLength < 129) {
     // if user input is legal, run function
-    generatePassword()
+    // password is written to the page
+    selectionLowerCase()
     // if user input is illegal, alert user to try again
   } else {
-    alert('Please Try Again')
+    console.log('invalid user input')
+    alert('Please enter a number between 8 - 128')
+  }
+}
+//if lowercase is desired, index 'lowercase' in selectionsArray, then move to next promt
+function selectionLowerCase() {
+  x = confirm('Include lowercase characters?')
+  if (x === true) {
+    x = "lowercase"
+    selectionsArray.push(x)
+    selectionUpperCase()
+  } else {
+    //if lowercase is not desired, move onto next prompt
+    selectionUpperCase()
+  }
+}
+//if uppercase is desired, index 'uppercase' in selectionsArray, then move to next prompt
+function selectionUpperCase() {
+  x = confirm('Include UPPERCASE characters?')
+  if (x === true) {
+    x = "UPPERCASE"
+    selectionsArray.push(x)
+    selectionNumber()
+  } else {
+    //if uppercase is not desired, index 'uppercase' in selectionsArray, then move to next prompt
+    selectionNumber()
+  }
+}
+//if number is desired, index 'number' in selectionsArray, then move to next prompt
+function selectionNumber() {
+  x = confirm('Include number characters?')
+  if (x === true) {
+    x = "number"
+    selectionsArray.push(x)
+    selectionSpecial()
+  } else {
+    //if number is not desired, move to next prompt
+    selectionSpecial()
+  }
+}
+//if special characters are desired, index 'special' in selectionsArray, then start generatePassword function
+function selectionSpecial() {
+  x = confirm('Include special characters?')
+  if (x === true) {
+    x = "special"
+    selectionsArray.push(x)
+    generatePassword()
+  } else {
+    //if special characters are not desired, move to generatePassword function
+    generatePassword()
   }
 }
 
 function generatePassword() {
-  
-  //define local function variables  
-  var alphaLowerChars = ('abcdefghijklmnopqrstuvwxyz')  
-  var alphaUpperChars = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')  
-  var numericChars = ('1234567890')  
-  var specialChars = ('!"#$%&*+-/;<=>?@\^_`|~')
-  var password = ''
-  var randomCharArr = []
-  var randomCharVal = ''
-  var userChoicesArr = []
-  var userInputArr = [] 
-  
-  //================================================
-  
-  //pick a random value from array 'x' and add it to our password string
-  function pickRndmChar(x) {
-    randomCharVal = Math.floor(Math.random() * x.length)
-    randomCharArr.push(x[randomCharVal])
-    password = password + (x[randomCharVal])
-    
+  if (selectionsArray.length === 0) {
+    //if user selects no options, then selectionsArray is empty, and user is alerted to try again
+    console.log('invalid user selections')
+    alert('!!!!--Please select character types!--!!!!')
   }
-  
- 
-  //================================================
-  
-  // record user input of password character options
-  alphaLower = confirm('Include lowercase characters?')   
-  alphaUpper = confirm('Include UPPERCASE characters?')
-  numeric = confirm('Include numbers?')
-  special = confirm('Include special characters?')  
-  
-  //run loop as many times as password length input
-  for (let index = 0; index < passwordLength; index++) { 
-    //include character option if selected & decrease password length by 1 if used
-    if (alphaLower === true) {
-      pickRndmChar(alphaLowerChars)
-      --passwordLength
+  //user has selected atleast 1 character type
+  else {
+    console.log('generating new password')
+    //generate the desired password length
+    for (let index = 0; index < passwordLength; index++) {
+      //generate random number to represent available index of selectionsArray
+      x = Math.floor(Math.random() * selectionsArray.length)
+      //if random number matches the 'lowercase' index, choose index. If it does not match, try next index
+      if (selectionsArray[x] === 'lowercase') {
+        //generate random number to choose a character index
+        l = Math.floor(Math.random() * alphaLowerChars.length)
+        //add the chosen character to the password string
+        password = password + (alphaLowerChars[l])
+      }
+      
+      //if random number matches the 'UPPERCASE' index, choose index. If it does not match, try next index
+      else if (selectionsArray[x] === 'UPPERCASE') {        
+        //generate random number to choose a character index
+        u = Math.floor(Math.random() * alphaUpperChars.length)
+        //add the chosen character to the password string
+        password = password + (alphaUpperChars[u])
+      }
+      
+      //if random number matches the 'number' index, choose index. If it does not match, try next index
+      else if (selectionsArray[x] === 'number') {
+        //generate random number to choose a character index
+        n = Math.floor(Math.random() * numericChars.length)
+        //add the chosen character to the password string
+        password = password + (numericChars[n])
+      }
+      
+      //if random number matches the 'special' index, choose index. If it does not match, try next index
+      else if (selectionsArray[x] === 'special') {
+        //generate random number to choose a character index
+        s = Math.floor(Math.random() * specialChars.length)
+        //add the chosen character to the password string
+        password = password + (specialChars[s])
+      }
     }
-    //include character option if selected & decrease password length by 1 if used
-    if (alphaUpper === true) {
-      pickRndmChar(alphaUpperChars)   
-      --passwordLength 
-    }
-    //include character option if selected & decrease password length by 1 if used
-    if (numeric === true) {
-      pickRndmChar(numericChars)
-      --passwordLength
-    }
-    //include character option if selected & decrease password length by 1 if used
-    if (special === true) {
-      pickRndmChar(specialChars)
-      --passwordLength
-    }
-    
+    //launch function
+    printToscreen()
   }
-  
-  // store our password here
-  var passwordText = document.querySelector("#password");
-  // password is written to the page
-  passwordText.value = password;
+
+  function printToscreen() {
+    //password is written to the page
+    passwordText.value = password;
+    //password is cleared from memory
+    password= ''
+    console.log('password erased from program')
+  }
 }
