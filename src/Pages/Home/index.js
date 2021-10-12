a3e8a1ed422ad279efcb697ea2935a92b9274162
Samuel Fox <samueljasonfox@gmail.com>
2021-10-12 14:59:55 -0700

    add: - sass in attemt to overwrite bootsrap defaults - css helpers - home page buttons
    update: excalidraw backup
    
    delete: react svg

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Container, Row, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import { PageContext } from '../../Context/PageContext'
 
 export default function Home() {
@@ -7,20 +7,23 @@ export default function Home() {
     const { setPage } = useContext(PageContext)
     console.log('rendering: Home')
     return (
-        <Container className="my-5 border">
+        <Container className="my-5">
             <Row className="tab-content" id="nav-tabContent">
                 <div className="tab-pane fade show active text-center" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                    <p>Welcome</p>
+                    <h1>Welcome</h1>
                     <p>to your personal lock-box</p>
                     <p>securely create and store</p>
                     <p>passwords and private text</p>
                 </div>
             </Row>
-            <Row>
-                <Container className="d-flex justify-content-center border">
-                    <Button>Testing</Button>
-                </Container>
+            <Row className="justify-content-center" >
+                <Col xs={8} md={4} className="border-orange mt-5">
+                    <Button variant="outline-info text-dark" className="w-100 my-3">create new chain</Button>
+                    <Button variant="outline-info text-dark" className="w-100 my-3">access existing chain</Button>
+                    <Button variant="outline-info text-dark" className="w-100 my-3">load from backup</Button>
+                </Col>
             </Row>
-        </Container>
+
+        </Container >
     )
 }
