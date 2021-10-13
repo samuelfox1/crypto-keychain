830c2470206817b9dd7a2108d9464c8b9a26d50a
Samuel Fox <samueljasonfox@gmail.com>
2021-10-13 01:33:10 -0700

    add: container for page components

--- src/Components/CustomNav/index.js
+++ src/Components/CustomNav/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 import NavTabs from '../NavTabs'
 
 
 export default function CustomNav() {
     return (
-        <nav className="w-100 d-flex justify-content-between align-items-end mb-5">
-            <h1 id="title" className="col-xs-12 col-sm-3 my-2 fs-6">crypto <span className="fs-1">
-                <span className="text-decoration-underline">key</span>chain</span>
-            </h1>
-            <NavTabs />
+        <nav className="w-100 mb-5">
+            <Container fluid>
+                <Row className="d-flex justify-content-between align-items-end ">
+                    <Col sx={12} sm={4}>
+                        <h1 id="title" className="my-2 fs-6">crypto <span className="fs-1">
+                            <span className="text-decoration-underline">key</span>chain</span>
+                        </h1>
+                    </Col>
+                    <Col sx={12} sm={8}>
+                        <NavTabs />
+                    </Col>
+                </Row>
+            </Container>
         </nav>
     )
 }
