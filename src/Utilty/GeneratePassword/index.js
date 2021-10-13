08537a4dcfc7fffb0d86504f8759b15394dac449
Samuel Fox <samueljasonfox@gmail.com>
2021-10-12 23:41:06 -0700

    add: incorporate password generator utility

---
+++ src/Utilty/GeneratePassword/index.js
@@ -0,0 +1,39 @@
+const options = {
+    number: '0123456789',
+    special: `!#$%&*+-/;<=>?@^_'|~\\`,
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: `ABCDEFGHIJKLMNOPQRSTUVWXYZ`
+};
+
+
+const generatePasswordString = ({ values, length }) => {
+    let str = '';
+    let includedTypes = [];
+
+    const randomNum = (len) => Math.floor(Math.random() * len);
+
+    for (let i = 0; i < length; i++) {
+        const randomKey = values[randomNum(values.length)];
+        str += options[randomKey][randomNum(options[randomKey].length)];
+        if (includedTypes.indexOf(randomKey) === -1) includedTypes.push(randomKey);
+    };
+
+    if (includedTypes.length === values.length) return str;
+
+    return generatePasswordString({ values, length });
+}
+
+// export const focusAndCopyToCb = (el) => {
+//     const textVal = el.text().trim()
+//     if (!textVal) return
+//     el.select() && el.focus();
+//     copyToCb(textVal)
+// };
+
+// export const copyToCb = (text) => {
+//     navigator.clipboard.writeText(text)
+//         .then(() => { });
+// }
+
+
+export const generatePassword = (config) => generatePasswordString(config);
