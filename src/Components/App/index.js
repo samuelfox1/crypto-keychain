dd6c9b14417c2739afdc297a1fe54f29e81a9e94
Samuel Fox <samueljasonfox@gmail.com>
2021-10-13 11:05:30 -0700

    update: Move App.xx files to its own component folder

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,21 +1,23 @@
 // import logo from './logo.svg';
 import React, { useContext } from 'react'
-import { PageContext } from './Context/PageContext'
-import CustomNav from './Components/CustomNav'
-import './App.css';
 import { Container } from 'react-bootstrap';
+import { AppContext } from '../../Context/'
+import CustomNav from '../CustomNav'
+import Footer from '../Footer';
+import './App.css';
 
 function App() {
+  const { content } = useContext(AppContext)
 
-  const { page } = useContext(PageContext)
   console.log('rendering: App')
+
   return (
     <>
       <CustomNav />
       <Container className="pb-1">
-        {page}
+        {content}
       </Container>
-      <h6 id="signature">_samuel fox <small>2021</small></h6>
+      <Footer />
     </>
   );
 }
