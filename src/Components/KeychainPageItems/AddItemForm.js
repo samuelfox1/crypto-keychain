c5655dd5cfee704e1c41b1d12ed6b865e59d2eb4
Samuel Fox <samueljasonfox@gmail.com>
2021-10-19 16:46:35 -0700

    add: -controlled form -suggest password button

--- src/Components/KeychainPageItems/AddItemForm.js
+++ src/Components/KeychainPageItems/AddItemForm.js
@@ -1,27 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Form, Row, Button } from 'react-bootstrap'
+import { createPassword } from '../../Utilty'
 
 export default function AddItemForm() {
+
+    const [validInputs, setValidInputs] = useState(false)
+    const [inputs, setInputs] = useState({
+        name: '',
+        value: ''
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+        setInputs({ ...inputs, [name]: value })
+    }
+
+    const handleSuggestPassword = () => {
+        setInputs({ ...inputs, value: createPassword() })
+    }
+
+    const handleAddKeychainItem = () => {
+        console.log(inputs)
+    }
+
+    useEffect(() => {
+        if (!inputs.name || !inputs.value) return setValidInputs(false)
+        if (!validInputs) setValidInputs(true)
+    }, [inputs])
+
     return (
         <Row>
-            <Col>
+            <Col className="p-0 m-0">
                 <Form>
                     <Form.Group className="mb-3" controlId="keychainItemName">
                         <Form.Label>name</Form.Label>
-                        <Form.Control type="text" />
+                        <Form.Control type="text" name='name' value={inputs.name} onChange={handleInputChange} />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="keychainItemValue">
                         <Form.Label>value</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
+                        <Form.Control
+                            className="text-center"
+                            as="textarea"
+                            rows={8}
+                            name='value'
+                            size="sm"
+                            value={inputs.value}
+                            style={{ resize: "none" }}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+                    <Form.Group className="d-flex justify-content-center mb-3">
+                        <Button className="p-0 px-1" variant="outline-warning text-dark" size="sm" onClick={handleSuggestPassword}>
+                            suggest password?
+                        </Button>
                     </Form.Group>
                     <Form.Group className="d-flex justify-content-end">
-                        <Button
-                            className="my-3"
-                            variant="outline-warning text-dark"
-                            onClick={() => console.log('submit')}
-                        >
-                            add item
-                        </Button>
+                        {validInputs
+                            ? <Button
+                                className="my-3"
+                                variant="outline-warning text-dark"
+                                onClick={(inputs) => handleAddKeychainItem(inputs)}
+                            >
+                                add item
+                            </Button>
+                            : <Button disabled className="my-3" variant="outline-warning text-dark" >add item</Button>
+                        }
                     </Form.Group>
                 </Form>
 
