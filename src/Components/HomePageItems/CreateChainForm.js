3675fdfa0ac9e3d303142054f9de47ecda595382
Samuel Fox <samueljasonfox@gmail.com>
2021-10-20 11:02:03 -0700

    update: layout consistency

--- src/Components/HomePageItems/CreateChainForm.js
+++ src/Components/HomePageItems/CreateChainForm.js
@@ -65,88 +65,89 @@ export default function CreateChain() {
     console.log('rendering: CreateChain')
 
     return (
-        <>
-            <Row className="justify-content-center">
-                <Col xs={10} md={4} >
-                    <h1>Create new chain</h1>
-                    <ol>
-                        <li><p >enter a name</p></li>
-                        <li><p >enter a password</p></li>
-                        <li><p>click create</p></li>
-                    </ol>
-                </Col>
-            </Row>
-            <Row className="justify-content-center mb-5" >
-                <Col xs={10} md={4} className="border-orange">
-                    <Form className="mt-3">
+        <Row className="justify-content-center">
+            <Col xs={10} md={6} className="border-orange shadow p-4">
+                <Row >
+                    <Col  >
+                        <h1>Create new chain</h1>
+                        <ol>
+                            <li><p >enter a name</p></li>
+                            <li><p >enter a password</p></li>
+                            <li><p>click create</p></li>
+                        </ol>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="border px-0 mx-2">
+                        <Form className="mt-3 mx-3">
 
-                        <Form.Group className="mb-3" controlId="keychainName">
-                            <Form.Label>keychain name:</Form.Label>
-                            <p className="error mb-0">{errorMessage.name}</p>
-                            <Form.Control
-                                size="sm"
-                                type="input"
-                                name="name"
-                                value={formInputs.name}
-                                onChange={handleInputChange}
-                            />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="keychainName">
+                                <Form.Label>keychain name: </Form.Label>
+                                <p className="error mb-0">{errorMessage.name}</p>
+                                <Form.Control
+                                    size="sm"
+                                    type="input"
+                                    name="name"
+                                    value={formInputs.name}
+                                    onChange={handleInputChange}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="keychainPassword">
-                            <Form.Label>password:</Form.Label>
-                            <p className="error mb-0">{errorMessage.pwlength}</p>
-                            <Form.Control
-                                size="sm"
-                                type="password"
-                                name="password"
-                                value={formInputs.password}
-                                onChange={handleInputChange}
-                            />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="keychainPassword">
+                                <Form.Label>password: </Form.Label>
+                                <p className="error mb-0">{errorMessage.pwlength}</p>
+                                <Form.Control
+                                    size="sm"
+                                    type="password"
+                                    name="password"
+                                    value={formInputs.password}
+                                    onChange={handleInputChange}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="kychainConfirmPassword">
-                            <Form.Label>confirm password: </Form.Label>
-                            <p className="error mb-0">{errorMessage.pwmatch}</p>
-                            <Form.Control
-                                size="sm"
-                                type="password"
-                                name="confirmPassword"
-                                value={formInputs.confirmPassword}
-                                onChange={(handleInputChange)}
-                            />
-                        </Form.Group>
+                            <Form.Group className="mb-3" controlId="kychainConfirmPassword">
+                                <Form.Label>confirm password: </Form.Label>
+                                <p className="error mb-0">{errorMessage.pwmatch}</p>
+                                <Form.Control
+                                    size="sm"
+                                    type="password"
+                                    name="confirmPassword"
+                                    value={formInputs.confirmPassword}
+                                    onChange={(handleInputChange)}
+                                />
+                            </Form.Group>
 
-                        <Form.Group className="d-flex justify-content-between">
-                            <Button
-                                variant="outline-dark"
-                                className="my-3"
-                                onClick={() => updateHomeComponent('DefaultLayout')}
-                            >
-                                back
-                            </Button>
-
-                            {validFormInputs
-                                ? <Button
-                                    variant="outline-warning text-dark"
+                            <Form.Group className="d-flex justify-content-between">
+                                <Button
+                                    variant="outline-dark"
                                     className="my-3"
-                                    onClick={handleCreateChain}
+                                    onClick={() => updateHomeComponent('DefaultLayout')}
                                 >
-                                    create
+                                    back
                                 </Button>
-                                : <Button
-                                    disabled
-                                    variant="outline-warning text-dark"
-                                    className="my-3"
-                                >
-                                    create
-                                </Button>
-                            }
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
-            <h2 className="text-center">new features coming soon</h2>
-            <h6 className="text-center">(checkout the password generator!)</h6>
-        </>
+
+                                {validFormInputs
+                                    ? <Button
+                                        variant="outline-warning text-dark"
+                                        className="my-3"
+                                        onClick={handleCreateChain}
+                                    >
+                                        create
+                                    </Button>
+                                    : <Button
+                                        disabled
+                                        variant="outline-warning text-dark"
+                                        className="my-3"
+                                    >
+                                        create
+                                    </Button>
+                                }
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                </Row>
+
+            </Col>
+        </Row>
     )
 }
--- src/Components/HomePageItems/DefaultLayout.js
+++ src/Components/HomePageItems/DefaultLayout.js
@@ -5,35 +5,33 @@ import { HomeContext } from '../../Context'
 export default function DefaultLayout() {
     const { updateHomeComponent } = useContext(HomeContext)
     return (
-        <>
-            <Row className="justify-content-center">
-                <Col xs={10} md={4} >
-                    <h1>Welcome!</h1>
-                    <ul>
-                        <li><p>improve your digital security</p></li>
-                        <li><p>create new passwords or access keys</p></li>
-                        <li><p>add existing passwords or keys</p></li>
-                        <li><p>keep them safe in an encrypted keychain</p></li>
-                    </ul>
-                </Col>
-            </Row>
-            <Row className="justify-content-center mb-5" >
-                <Col xs={10} md={4} className="shadow">
-                    <Button
-                        variant="outline-warning text-dark"
-                        className="w-100 my-3"
-                        onClick={() => updateHomeComponent('createChain')}
-                    >
-                        create new chain
-                    </Button>
-                    <Button disabled variant="outline-warning text-dark" className="w-100 my-3">access existing chain</Button>
-                    <Button disabled variant="outline-warning text-dark" className="w-100 my-3">load from backup</Button>
-                </Col>
-            </Row>
-            <Row>
-                <h2 className="text-center">new features coming soon</h2>
-                <h6 className="text-center">(checkout the password generator!)</h6>
-            </Row>
-        </>
+        <Row className="justify-content-center">
+            <Col xs={11} md={6} className="shadow border-orange p-3">
+                <Row >
+                    <Col >
+                        <h1>Welcome!</h1>
+                        <ul>
+                            <li><p>improve your digital security</p></li>
+                            <li><p>create new passwords or access keys</p></li>
+                            <li><p>add existing passwords or keys</p></li>
+                            <li><p>keep them safe in an encrypted keychain</p></li>
+                        </ul>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="mx-2">
+                        <Button
+                            variant="outline-warning text-dark"
+                            className="w-100 my-3"
+                            onClick={() => updateHomeComponent('createChain')}
+                        >
+                            create new chain
+                        </Button>
+                        <Button disabled variant="outline-warning text-dark" className="w-100 my-3">access existing chain</Button>
+                        <Button disabled variant="outline-warning text-dark" className="w-100 my-3">load from backup</Button>
+                    </Col>
+                </Row>
+            </Col >
+        </Row >
     )
 }
